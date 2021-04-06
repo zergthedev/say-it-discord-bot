a552795602b9c3ca1d1df1f8f00694c90469a03e
zergthedev <60578485+zergthedev@users.noreply.github.com>
2021-04-06 17:45:56 +0100

    Update

---
+++ main.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.';
+
+client.once('ready', () =>{
+	console.log('Bot is alive!');
+});
+
+client.on('message', message =>{
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+	
+	// pomoc
+	if(command === 'say'){
+		const title = args.slice(1).join(' ')
+		const desc = args.slice(2).join(' ')
+		.setTitle(title)
+		.setDescription(desc)
+		.setColor('#00BE84')
+		message.channel.send()
+	} 
+});
+
+client.login(process.env.token);
